3e736ce67df37462aa9da942a98aff84526ef605
TheRadDani <ldanielfch@gmail.com>
2025-04-29 10:13:59 -0500

    sigmoid function and derv

--- Class12/example1.c
+++ Class12/example1.c
@@ -168,6 +168,16 @@ NeuralNetwork* create_neuronal_nertwork(const size_t* layer_sizes, size_t num_la
     return network;
 }
 
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
+
+double sigmoid_derivative(double x) {
+    return x * (1.0 - x);
+}
+
+
+
 
 int main() {
 
