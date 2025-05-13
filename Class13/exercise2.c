51b428a97145b2b3c7c497071f02855daffc57c3
TheRadDani <ldanielfch@gmail.com>
2025-05-13 09:30:29 -0500

    Class13

--- Class13/exercise2.c
+++ Class13/exercise2.c
@@ -1,12 +1,17 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int compare(const void* a, const void* b) {
-    
+    return (*(int*)a - *(int*)b);
 }
 
 int main() {
-    int arr = {5, 2, 8, 1, 4};
+    int arr[5] = {5, 2, 8, 1, 4};
     qsort(arr, 5, sizeof(int), compare);
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
     // arr {1, 2, 4, 5, 8}
     return 0;
 }
